9eae5add69e102b68294ebdbc71ef0c18551dfa2
Hung <>
2022-06-23 12:02:14 +0800

    [update] firework test#2

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "e408938f7c458d7b46990a39217583b8",
-"index.html": "a95b84465359db6e7a89f54133219a09",
-"/": "a95b84465359db6e7a89f54133219a09",
-"main.dart.js": "6f6da4574e05f4163b77e4616d522e3c",
+"index.html": "d8ea25c297dd33d12bbdf4f016022e40",
+"/": "d8ea25c297dd33d12bbdf4f016022e40",
+"main.dart.js": "4e4518562d5f56aae26721cb6a9facb0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
